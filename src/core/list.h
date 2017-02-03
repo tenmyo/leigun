62e8723111658e0e065013cd7e9c3dc6f93fca39
tenmyo <tenmyo@gmail.com>
2017-02-03 16:47:12 +0900

    [ADD] Add core/list.h

---
+++ src/core/list.h
@@ -0,0 +1,98 @@
+//===-- core/list.h - Generic list data structure manupulation ----*- C -*-===//
+//
+//              The Leigun Embedded System Simulator Platform
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+//===----------------------------------------------------------------------===//
+///
+/// \file
+/// This file contains the declaration of the Generic list structure and
+/// manupulation macros.
+///
+/// \ attention
+/// THREAD UNSAFE.
+///
+//===----------------------------------------------------------------------===//
+#pragma once
+#ifdef __cplusplus
+extern "C" {
+#endif
+//==============================================================================
+//= Dependencies
+//==============================================================================
+
+
+//==============================================================================
+//= Constants(also Enumerations)
+//==============================================================================
+
+
+//==============================================================================
+//= Types
+//==============================================================================
+typedef struct List_s List_t;
+typedef struct List_Element_s List_Element_t;
+struct List_s {
+    List_Element_t *l_head;
+};
+struct List_Element_s {
+    List_Element_t *l_next;
+};
+
+
+//==============================================================================
+//= Variables
+//==============================================================================
+
+
+//==============================================================================
+//= Macros
+//==============================================================================
+#define List_Members(member_t) member_t *l_head;
+
+#define List_ElementMembesr(member_t) member_t *l_next;
+
+#define List_Init(l)                                                           \
+    do {                                                                       \
+        (l)->l_head = NULL;                                                    \
+    } while (0)
+
+#define List_InitElement(e)                                                    \
+    do {                                                                       \
+        (e)->l_next = NULL;                                                    \
+    } while (0)
+
+#define List_Push(l, e)                                                        \
+    do {                                                                       \
+        (e)->l_next = (l)->l_head;                                             \
+        (l)->l_head = e;                                                       \
+    } while (0)
+
+#define List_Map(member_t, l, proc)                                            \
+    do {                                                                       \
+        member_t *element, *next; /* NOLINT for member_t parentheses */        \
+        for (element = (l)->l_head; element; element = next) {                 \
+            next = element->l_next;                                            \
+            proc(element);                                                     \
+        }                                                                      \
+    } while (0)
+
+
+//==============================================================================
+//= Functions
+//==============================================================================
+
+#ifdef __cplusplus
+}
+#endif
