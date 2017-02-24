05dac8b9e05d6f7c858ce151af97d9c97b91c678
TENMYO Masakazu <tenmyo@gmail.com>
2017-02-24 17:35:47 +0900

    [BUILD] creanup-recursive exclude softgun's file

--- dev-scripts/cleanup-recuraive.sh
+++ dev-scripts/cleanup-recuraive.sh
@@ -3,8 +3,8 @@
  
 # note: clang-tidy only accepts one file at a time, but we can run it
 #       parallel against disjoint collections at once.
-find . \( -name \*.c -or -name \*.cpp -or -name \*.cc \) |xargs -n1 -P4 $(dirname $0)/cleanup-tidy.sh
+find . -name softgun -prune -o \( -name \*.c -or -name \*.cpp -or -name \*.cc \) |xargs -n1 -P4 $(dirname $0)/cleanup-tidy.sh
 
 # clang-format accepts multiple files during one run, but let's limit it to 12
 # here so we (hopefully) avoid excessive memory usage.
-find . \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) |xargs -n12 -P4 $(dirname $0)/cleanup-format.sh -i
+find . -name softgun -prune -o \( -name \*.c -or -name \*.cpp -or -name \*.cc -or -name \*.h \) |xargs -n12 -P4 $(dirname $0)/cleanup-format.sh -i
